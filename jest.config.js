a3cac135fd4de89c85de4cd9c8872053a75e0088
Gabe <gabriel.riera@sharesight.co.nz>
2022-08-25 15:40:48 +1200

    WIP: attept at replacing our package json version fetch from graphql to a different package

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,21 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["./jest.setup.js"],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    // "node_modules/package-json/.+\\.(js|jsx)?$": "ts-jest"
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!package-json)"],
+  // moduleFileExtensions: [
+  //   "ts",
+  //   "tsx",
+  //   "js",
+  //   "jsx",
+  //   "json",
+  //   "node"
+  // ],
+  // moduleDirectories: [
+  //   "node_modules",
+  //   "src"
+  // ],
 };
--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
-import { graphql } from '@octokit/graphql';
-import type { GraphQlQueryResponseData } from '@octokit/graphql';
+// import { graphql } from '@octokit/graphql';
+// import type { GraphQlQueryResponseData } from '@octokit/graphql';
 
 import fs from 'fs';
 import path from 'path';
@@ -8,6 +8,7 @@ import { setOutput } from '@actions/core';
 
 import { getConfig } from './config';
 import type { Config } from './config';
+import packageJson from 'package-json';
 
 interface PackageJson {
   version: string;
@@ -35,68 +36,69 @@ export function setPackageJsonVersion(config: Config = getConfig()): void {
   setOutput('current_version', version);
 }
 
-async function fetchLatestVersion(
-  config: Config = getConfig()
-): Promise<GraphQlQueryResponseData> {
-  const [owner, repo] = config.repository.split('/');
+// async function fetchLatestVersion(
+//   config: Config = getConfig()
+// ): Promise<GraphQlQueryResponseData> {
+//   const [owner, repo] = config.repository.split('/');
 
-  let authorization;
-  if (process.env.GITHUB_TOKEN) {
-    authorization = `token ${process.env.GITHUB_TOKEN}`;
-  }
+//   let authorization;
+//   if (process.env.GITHUB_TOKEN) {
+//     authorization = `token ${process.env.GITHUB_TOKEN}`;
+//   }
 
-  try {
-    return await graphql(
-      `
-        query getLatestVersion($owner: String!, $repo: String!) {
-          repository(owner: $owner, name: $repo) {
-            packages(first: 2) {
-              nodes {
-                latestVersion {
-                  id
-                  version
-                }
-              }
-            }
-          }
-        }
-      `,
-      {
-        owner,
-        repo,
-        headers: {
-          authorization,
-        },
-      }
-    );
-  } catch (err) {
-    if (err.status === 401) {
-      let message = `⚠️ ${err.status}: Authentication failed!`;
-      if (!authorization) {
-        message += ' You should provide a `GITHUB_TOKEN` env.';
-      }
+//   try {
+//     return await graphql(
+//       `
+//         query getLatestVersion($owner: String!, $repo: String!) {
+//           repository(owner: $owner, name: $repo) {
+//             packages(first: 2) {
+//               nodes {
+//                 latestVersion {
+//                   id
+//                   version
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       `,
+//       {
+//         owner,
+//         repo,
+//         headers: {
+//           authorization,
+//         },
+//       }
+//     );
+//   } catch (err) {
+//     if (err.status === 401) {
+//       let message = `⚠️ ${err.status}: Authentication failed!`;
+//       if (!authorization) {
+//         message += ' You should provide a `GITHUB_TOKEN` env.';
+//       }
 
-      throw new Error(
-        `${message} Received error from Github GraphQL: "${err.message}"`
-      );
-    }
+//       throw new Error(
+//         `${message} Received error from Github GraphQL: "${err.message}"`
+//       );
+//     }
 
-    throw err;
-  }
-}
+//     throw err;
+//   }
+// }
 
 export async function getLatestRemoteVersion(
   config: Config = getConfig()
 ): Promise<string> {
-  const response: GraphQlQueryResponseData = await fetchLatestVersion(config);
+  const response = await packageJson('@sharesight/react');
+  console.log("@@@response", response);
 
-  if (response.repository.packages.nodes.length > 1) {
+  if (Object.keys(response.versions).length > 1) {
     throw new Error(
-      `🚧 Found ${response.repository.packages.nodes.length} packages on '${config.repository}', expected only 1.  Monorepo/multiple packages is not supported yet.`
+      `🚧 Found ${Object.keys(response.versions).length} packages on '${config.repository}', expected only 1.  Monorepo/multiple packages is not supported yet.`
     );
   }
 
-  const version = response.repository.packages.nodes[0].latestVersion.version;
+  const version = response.versions['version'].version;
 
   if (!version) {
     throw new Error(
