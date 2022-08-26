e65dd16bd224d4f26b115db6a09cb76f38d7b879
Gabe <gabriel.riera@sharesight.co.nz>
2022-08-26 14:31:34 +1200

    refactor: revert config code for module changes

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,4 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["./jest.setup.js"],
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    // "node_modules/package-json/.+\\.(js|jsx)?$": "ts-jest"
-  },
-  transformIgnorePatterns: ["<rootDir>/node_modules/(?!package-json)"],
-  // moduleFileExtensions: [
-  //   "ts",
-  //   "tsx",
-  //   "js",
-  //   "jsx",
-  //   "json",
-  //   "node"
-  // ],
-  // moduleDirectories: [
-  //   "node_modules",
-  //   "src"
-  // ],
 };
